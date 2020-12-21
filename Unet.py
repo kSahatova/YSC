9e924ff155a69bf26c9245371ff632f7706e6b39
kSahatova <60181622+kSahatova@users.noreply.github.com>
2020-12-21 20:08:15 +0300

    Add files via upload

---
+++ Unet.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+import os
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("ggplot")
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+from tqdm import tqdm_notebook, tnrange
+from itertools import chain
+from skimage.io import imread, imshow, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+from sklearn.model_selection import train_test_split
+
+import tensorflow as tf
+
+from keras.models import Model, load_model
+from keras.layers import Input, BatchNormalization, Activation, Dense, Dropout
+from keras.layers.core import Lambda, RepeatVector, Reshape
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D, GlobalMaxPool2D
+from keras.layers.merge import concatenate, add
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+
+# In[16]:
+
+
+'''import shutil, os
+
+def copytoSub(filename, zoom):
+    
+
+    for i in range (1, 6):
+  
+        # path 
+        path = 'mask'
+        # Source path 
+        source = "mask/no_oil_reservoir0.png"
+        # Print the metadeta 
+        # of source file 
+        metadata = os.stat(source) 
+        # Destination path 
+        destination = "mask/no_oil_reservoir"+str(i)+'.png'
+        # Copy the content of 
+        # source to destination 
+        dest = shutil.copy2(source, destination)
+        # Print the metadata 
+        # of the destination file 
+        matadata = os.stat(destination) 
+       
+ 
+copytoSub("mask",358 )
+'''
+
+
+# In[63]:
+
+
+im_width = 640
+im_height = 400
+border = 5
+
+
+# In[64]:
+
+
+ids = next(os.walk("images"))[2] # list of names all images in the given path
+print("No. of images = ", len(ids))
+
+
+# In[65]:
+
+
+X = np.zeros((len(ids), im_height, im_width, 1), dtype=np.float32)
+y = np.zeros((len(ids), im_height, im_width, 1), dtype=np.float32)
+
+
+# In[66]:
+
+
+for n, id_ in enumerate(ids):
+    # Load images
+    img = load_img("images/"+id_, grayscale=True)
+    x_img = img_to_array(img)
+    x_img = resize(x_img, ( im_height,im_width, 1), mode = 'constant', preserve_range = True)
+    # Load masks
+    mask = img_to_array(load_img("mask/"+id_, grayscale=True))
+    mask = resize(mask, ( im_height,im_width, 1), mode = 'constant', preserve_range = True)
+    # Save images
+    X[n] = x_img/255.0
+    y[n] = mask/255.0
+
+
+# In[67]:
+
+
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.1, random_state=42)
+
+
+# In[68]:
+
+
+def conv2d_block(input_tensor, n_filters, kernel_size = 3, batchnorm = True):
+    """Function to add 2 convolutional layers with the parameters passed to it"""
+    # first layer
+    x = Conv2D(filters = n_filters, kernel_size = (kernel_size, kernel_size),              kernel_initializer = 'he_normal', padding = 'same')(input_tensor)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    # second layer
+    x = Conv2D(filters = n_filters, kernel_size = (kernel_size, kernel_size),              kernel_initializer = 'he_normal', padding = 'same')(input_tensor)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    return x
+
+
+# In[69]:
+
+
+
+def get_unet(input_img, n_filters = 16, dropout = 0.1, batchnorm = True):
+    """Function to define the UNET Model"""
+    # Contracting Path
+    c1 = conv2d_block(input_img, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
+    p1 = MaxPooling2D((2, 2))(c1)
+    p1 = Dropout(dropout)(p1)
+    
+    c2 = conv2d_block(p1, n_filters * 2, kernel_size = 3, batchnorm = batchnorm)
+    p2 = MaxPooling2D((2, 2))(c2)
+    p2 = Dropout(dropout)(p2)
+    
+    c3 = conv2d_block(p2, n_filters * 4, kernel_size = 3, batchnorm = batchnorm)
+    p3 = MaxPooling2D((2, 2))(c3)
+    p3 = Dropout(dropout)(p3)
+    
+    c4 = conv2d_block(p3, n_filters * 8, kernel_size = 3, batchnorm = batchnorm)
+    p4 = MaxPooling2D((2, 2))(c4)
+    p4 = Dropout(dropout)(p4)
+    
+    c5 = conv2d_block(p4, n_filters = n_filters * 16, kernel_size = 3, batchnorm = batchnorm)
+    
+    # Expansive Path
+    u6 = Conv2DTranspose(n_filters * 8, (3, 3), strides = (2, 2), padding = 'same')(c5)
+    u6 = concatenate([u6, c4])
+    u6 = Dropout(dropout)(u6)
+    c6 = conv2d_block(u6, n_filters * 8, kernel_size = 3, batchnorm = batchnorm)
+    
+    u7 = Conv2DTranspose(n_filters * 4, (3, 3), strides = (2, 2), padding = 'same')(c6)
+    u7 = concatenate([u7, c3])
+    u7 = Dropout(dropout)(u7)
+    c7 = conv2d_block(u7, n_filters * 4, kernel_size = 3, batchnorm = batchnorm)
+    
+    u8 = Conv2DTranspose(n_filters * 2, (3, 3), strides = (2, 2), padding = 'same')(c7)
+    u8 = concatenate([u8, c2])
+    u8 = Dropout(dropout)(u8)
+    c8 = conv2d_block(u8, n_filters * 2, kernel_size = 3, batchnorm = batchnorm)
+    
+    u9 = Conv2DTranspose(n_filters * 1, (3, 3), strides = (2, 2), padding = 'same')(c8)
+    u9 = concatenate([u9, c1])
+    u9 = Dropout(dropout)(u9)
+    c9 = conv2d_block(u9, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
+    
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+    model = Model(inputs=[input_img], outputs=[outputs])
+    return model
+
+
+# In[74]:
+
+
+input_img = Input((  im_height,im_width,1), name='img')
+model = get_unet(input_img, n_filters=16, dropout=0.05, batchnorm=True)
+model.compile(optimizer=Adam(), loss="binary_crossentropy", metrics=["accuracy"])
+
+
+# In[75]:
+
+
+model.summary()
+
+
+# In[76]:
+
+
+callbacks = [
+    EarlyStopping(patience=10, verbose=1),
+    ReduceLROnPlateau(factor=0.1, patience=5, min_lr=0.00001, verbose=1),
+    ModelCheckpoint('model-tgs-salt.h5', verbose=1, save_best_only=True, save_weights_only=True)
+]
+
+
+# In[79]:
+
+
+results = model.fit(X_train, y_train, batch_size=32, epochs=50, callbacks=callbacks,                    validation_data=(X_valid, y_valid))
+
+
+# In[80]:
+
+
+plt.figure(figsize=(8, 8))
+plt.title("Learning curve")
+plt.plot(results.history["loss"], label="loss")
+plt.plot(results.history["val_loss"], label="val_loss")
+plt.plot( np.argmin(results.history["val_loss"]), np.min(results.history["val_loss"]), marker="x", color="r", label="best model")
+plt.xlabel("Epochs")
+plt.ylabel("log_loss")
+plt.legend();
+
+
+# In[81]:
+
+
+# load the best model
+model.load_weights('model-tgs-salt.h5')
+
+
+# In[82]:
+
+
+# Evaluate on validation set (this must be equals to the best log_loss)
+model.evaluate(X_valid, y_valid, verbose=1)
+
+
+# In[83]:
+
+
+# Predict on train, val and test
+preds_train = model.predict(X_train, verbose=1)
+preds_val = model.predict(X_valid, verbose=1)
+
+
+# In[84]:
+
+
+# Threshold predictions
+preds_train_t = (preds_train > 0.5).astype(np.uint8)
+preds_val_t = (preds_val > 0.5).astype(np.uint8)
+
+
+# In[ ]:
+
+
+
+
